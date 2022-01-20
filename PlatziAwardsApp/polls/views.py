534d61cfda784e9f8e70b50091451a2806604849
Juan David Arce Martinez <juandavid.arce@utp.edu.co>
2022-01-20 16:22:07 -0500

    templates

--- PlatziAwardsApp/polls/views.py
+++ PlatziAwardsApp/polls/views.py
@@ -1,7 +1,29 @@
-from django.shortcuts import render
+# Django
+from django.shortcuts import render,get_object_or_404
 from django.http import HttpResponse
-# Create your views here.
+
+
+# Models
+from .models import Question
+
 
 
 def index(request):
-    return HttpResponse("Hello world!")
+    latest_question_list = Question.objects.all()
+    return render(request,"polls/index.html",context={
+        "latest_question_list":latest_question_list
+    })
+
+
+def detail(request,question_id):
+    question =get_object_or_404(Question,pk=question_id)
+    return render(request,"polls/detail.html",context={
+        "question":question
+    })
+
+def results(request,question_id):
+    return HttpResponse(f"You are seen results of question number {question_id}")
+
+
+def vote(request,question_id):
+    return HttpResponse(f"You are votting question number {question_id}")
